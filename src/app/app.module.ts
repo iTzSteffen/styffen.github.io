cea2463103692b9149dc529aca945abfbbe3dfb7
Steffen Hofbauer <47820801+iTzSteffen@users.noreply.github.com>
2024-03-16 23:39:11 +0100

    udpated base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BlogComponent } from './views/blog/blog.component';
+import { PrivacyPolicyComponent } from './views/privacy-policy/privacy-policy.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MenuComponent,
+    BlogComponent,
+    PrivacyPolicyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
