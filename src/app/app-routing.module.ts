a5564c03a1dd0fe78780cbf821aa249b184f759f
Steffen Hofbauer <47820801+iTzSteffen@users.noreply.github.com>
2024-03-17 09:20:41 +0100

    init portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
-import { AboutComponent } from './views/about/about.component';
-import { BlogComponent } from './views/blog/blog.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'blog', component: BlogComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
-import { AboutComponent } from './views/about/about.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { BlogComponent } from './views/blog/blog.component';
 import { PrivacyPolicyComponent } from './views/privacy-policy/privacy-policy.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -14,9 +12,7 @@ import { FooterComponent } from './components/footer/footer.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
     MenuComponent,
-    BlogComponent,
     PrivacyPolicyComponent,
     FooterComponent
   ],
