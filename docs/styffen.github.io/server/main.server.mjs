9741504cac404ca5ba8730068a60324f929e1ec9
Steffen Hofbauer <steffen.hofbauer@basf.com>
2024-10-21 12:04:09 +0200

    renamed dist to docs

---
+++ docs/styffen.github.io/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{b as a}from"./chunk-GDKDZ353.mjs";import"./chunk-2YVZ5AIN.mjs";import"./chunk-JQ6GOPGT.mjs";import"./chunk-WUI6SWGE.mjs";export{a as default};
